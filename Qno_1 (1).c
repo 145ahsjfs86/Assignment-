21896b968d93b836f86042f88a607602bddbc29b
145ahsjfs86 <133495710+145ahsjfs86@users.noreply.github.com>
2023-09-11 15:49:27 +0500

    Assignment of programming
    Amna Ibrahim roll no 2k23/BLCS11

---
+++ Qno_1 (1).c
@@ -0,0 +1,11 @@
+//program to print your Name, father name,age and grade
+
+#include<stdio.h>
+
+int main (){
+printf("Name:Amna\n");
+printf("Father's name: Muhammad Ibrahim\n");
+printf("Age:19\n");
+printf("Grade:A\n");
+return 0;
+}
